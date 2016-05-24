4a6952d9556701ff615498e72cd97facb541c222
RicoLiu <2869183609@qq.com>
2016-05-24 20:10:28 +0800

    add three.js

--- app/javascripts/main/main.main-ctrl.js
+++ app/javascripts/main/main.main-ctrl.js
@@ -3,35 +3,73 @@
  */
 
 app.controller('loginCtrl', ['$scope',  '$rootScope', '$state', '$http', 'toaster', function ($scope, $rootScope, $state, $http, toaster) {
-  //$scope.login = function () {
-  //  $scope.addData = {
-  //    username: $scope.username,
-  //    password: $scope.password
-  //  }
-  //  $http.post('/api/login', $scope.addData)
-  //    .then(function (res) {
-  //      //handle data
-  //    }, function (res) {
-  //      toaster.pop('error', '登录失败，用户名或密码错误', null, 2000, 'toast-top-full-width-login');
-  //    });
+  //Three.js 基础
+
+  //var scene = new THREE.Scene();//场景
+  //
+  //var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);//相机
+  //
+  //var renderer = new THREE.WebGLRenderer();//渲染器
+  //renderer.setClearColor('#FFFFFF');//设置背景颜色
+  //
+  //renderer.setSize(window.innerWidth, window.innerHeight);
+  //
+  //document.body.appendChild(renderer.domElement);
+  //
+  //var geometry = new THREE.CubeGeometry(1,1,1);
+  //var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+  //var objectTotal = new THREE.Object3D();
+  //var cube = new THREE.Mesh(geometry,material);
+  //
+  //objectTotal.add(cube);
+  //
+  //camera.position.z = 5;//设置相机位置
+  //camera.position.x = 3;
+  //
+  //
+  //var axisHelper = new THREE.AxisHelper(4);
+  //objectTotal.add(axisHelper);
+  //
+  //scene.add(objectTotal);
+  //
+  //function render () {
+  //  requestAnimationFrame(render);
+  //  objectTotal.rotation.y += 0.01;
+  //  objectTotal.rotation.z += 0.1;
+  //  //console.log(objectTotal.rotation.y)
+  //  renderer.render(scene,camera)
   //}
+  //
+  //render();
+
+
+  //绘制点、线,三角形
+
+  var renderer;
+  var camera;
+  var scene;
+  var light;
+  var cube;
+  var line;
   var width;
   var height;
-  var renderer;
-  function initThree() {
+
+
+
+  function initThree () {
     width = document.getElementById('canvas-frame').clientWidth;
     height = document.getElementById('canvas-frame').clientHeight;
     renderer = new THREE.WebGLRenderer({
-      antialias : true
+      antialias: true
     });
     renderer.setSize(width, height);
     document.getElementById('canvas-frame').appendChild(renderer.domElement);
-    renderer.setClearColor(0xFFFFFF, 1.0);
+    renderer.setClearColor(0xffffff, 1.0);
   }
 
-  var camera;
-  function initCamera() {
-    camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+  function initCamera () {
+    camera = new THREE.PerspectiveCamera(45, width/height, 1, 10000);
+    //唯一的决定了相机的摆放位置
     camera.position.x = 0;
     camera.position.y = 0;
     camera.position.z = 600;
@@ -39,43 +77,54 @@ app.controller('loginCtrl', ['$scope',  '$rootScope', '$state', '$http', 'toaste
     camera.up.y = 1;
     camera.up.z = 0;
     camera.lookAt({
-      x : 0,
-      y : 0,
-      z : 0
-    });
+      x:0,
+      y:0,
+      z:0
+    })
   }
 
-  var scene;
-  function initScene() {
+  function initScene () {
     scene = new THREE.Scene();
   }
 
-  var light;
-  function initLight() {
-    light = new THREE.AmbientLight(0xFF0000);
+  function initLight () {
+    light = new THREE.DirectionalLight(0xff0000, 1.0, 0);
     light.position.set(100, 100, 200);
     scene.add(light);
-    light = new THREE.PointLight(0x00FF00);
-    light.position.set(0, 0,300);
-    scene.add(light);
   }
 
-  var cube;
-  var mesh;
-  function initObject() {
-    var geometry = new THREE.CylinderGeometry( 100,150,400);
-    var material = new THREE.MeshLambertMaterial( { color:0xFFFFFF} );
-    mesh = new THREE.Mesh( geometry,material);
-    mesh.position = new THREE.Vector3(0,0,0);
-    scene.add(mesh);
+  function initObject () {
+    var geometry = new THREE.Geometry();
+    var material = new THREE.LineBasicMaterial({ vertexColors:true });
+    var color1 = new THREE.Color( 0x444444 ), color2 = new THREE.Color( 0xff0000), color3 = new THREE.Color(0x00ff00);
+    var objectTotal = new THREE.Object3D();
+    var axisHelper = new THREE.AxisHelper(400);
+
+    var p1 = new THREE.Vector3(-200, 0, 0);
+    var p2 = new THREE.Vector3(0, 100, 0);
+    var p3 = new THREE.Vector3(0, 0, 100);
+
+    geometry.vertices.push(p1);
+    geometry.vertices.push(p2);
+    geometry.vertices.push(p3);
+    geometry.vertices.push(p1);
+
+    geometry.colors.push( color1, color2, color3, color1);
+
+    line = new THREE.Line( geometry, material, THREE.LineSegments );
+    objectTotal.add(line);
+    objectTotal.add(axisHelper);
+    scene.add(objectTotal);
   }
 
-  function animation()
-  {
-    mesh.position.x-=1;
+  function animation () {
+    //line.position.x -= 1;
+    camera.position.x += 10;
+    camera.position.z += 10;
     renderer.render(scene, camera);
     requestAnimationFrame(animation);
   }
+
 
     initThree();
     initCamera();
@@ -83,6 +132,9 @@ app.controller('loginCtrl', ['$scope',  '$rootScope', '$state', '$http', 'toaste
     initLight();
     initObject();
     animation();
+
+
+
 
 
 
