11d095b64caeb5db3f2a4134aeaf63223ed3ebda
RicoLiu <2869183609@qq.com>
2016-05-30 18:06:24 +0800

    dat.gui.min.js & stats.min.js

--- app/javascripts/main/main.main-ctrl.js
+++ app/javascripts/main/main.main-ctrl.js
@@ -45,6 +45,8 @@ app.controller('loginCtrl', ['$scope',  '$rootScope', '$state', '$http', 'toaste
 
   //绘制点、线,三角形
 
+  if (!Detector.webgl) Detector.addGetWebGLMessage()
+
   var renderer;
   var camera;
   var scene;
@@ -55,12 +57,15 @@ app.controller('loginCtrl', ['$scope',  '$rootScope', '$state', '$http', 'toaste
   var texture = null;
   var width;
   var height;
+  var mesh;
+  var stats;
 
   initThree();
   initCamera();
   initScene();
   initLight();
   initObject();
+  initGrid();
   createUI();
   animation();
 
@@ -76,17 +81,22 @@ app.controller('loginCtrl', ['$scope',  '$rootScope', '$state', '$http', 'toaste
     document.getElementById('canvas-frame').appendChild(renderer.domElement);
     renderer.setClearColor(0xffffff, 1.0);
 
+    stats = new Stats()
+    stats.domElement.style.position = 'absolute'
+    stats.domElement.style.top = '0px'
+    document.getElementById('canvas-frame').appendChild(stats.domElement)
+
     window.addEventListener('resize', onWindowResize, false)
   }
 
   function initCamera () {
-    camera = new THREE.PerspectiveCamera(45, width/height, 1, 10000);//透视投影相机
+    camera = new THREE.PerspectiveCamera(45, width/height, 1, 3500);//透视投影相机
     //camera = new THREE.OrthographicCamera(width / -2, width/2, height/2, height/-2, 10, 1000);//正投影相机
 
     //唯一的决定了相机的摆放位置
     camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 600;
+    camera.position.y = 800;
+    camera.position.z = 1600;
     camera.up.x = 0;
     camera.up.y = 1;
     camera.up.z = 0;
@@ -113,10 +123,16 @@ app.controller('loginCtrl', ['$scope',  '$rootScope', '$state', '$http', 'toaste
     gui.add(param,"y",-10000,10000).name("方向光Y的位置")
     gui.add(param,"z",-10000,10000).name("方向光Z的位置")
 
-    light = new THREE.DirectionalLight(0xffff00,1.0);
+    scene.add( new THREE.AmbientLight(0x444444) )
+
+    light = new THREE.DirectionalLight(0xffffff,0.5);
     //light = new THREE.DirectionalLight()
-    light.position.set(param.x, param.y, param.z);
+    light.position.set(1, 1, 1);
     scene.add(light);
+
+    var light1 = new THREE.DirectionalLight(0xffffff,1.5)
+    light1.position.set(0, 1, 0)
+    scene.add(light1)
   }
 
   function initObject () {
@@ -198,7 +214,7 @@ app.controller('loginCtrl', ['$scope',  '$rootScope', '$state', '$http', 'toaste
       }
     )*/
 
-    var geometry = new THREE.Geometry()
+    /*var geometry = new THREE.Geometry()
     var material = new THREE.MeshBasicMaterial({vertexColors: THREE.VertexColors,wireframe:false})
     var color1 = new THREE.Color(0x00900f)
     var color2 = new THREE.Color(0xdb4437)
@@ -220,7 +236,133 @@ app.controller('loginCtrl', ['$scope',  '$rootScope', '$state', '$http', 'toaste
 
     var mesh = new THREE.Mesh(geometry, material)
 
+    scene.add(mesh)*/
+
+    /*var triangles = 160000
+    var geometry = new THREE.BufferGeometry()
+    var positions = new Float32Array( triangles * 3 * 3 )//three points and x y z
+    var normals = new Float32Array( triangles * 3 * 3 )
+    var colors = new Float32Array( triangles * 3 * 3 )
+    var color = new THREE.Color()
+    var n = 800, n2 = n/2//triangles spread in the cube
+    var d = 12, d2 = d/2//individual triangle size
+
+    var pA = new THREE.Vector3()
+    var pB = new THREE.Vector3()
+    var pC = new THREE.Vector3()
+
+    var cb = new THREE.Vector3()
+    var ab = new THREE.Vector3()
+
+    for (var i = 0; i < positions.length; i += 9) {
+      var x = Math.random() * n - n2
+      var y = Math.random() * n - n2
+      var z = Math.random() * n - n2
+
+      var ax = x + Math.random() * d - d2
+      var ay = y + Math.random() * d - d2
+      var az = z + Math.random() * d - d2
+
+      var bx = x + Math.random() * d - d2
+      var by = y + Math.random() * d - d2
+      var bz = z + Math.random() * d - d2
+
+      var cx = x + Math.random() * d - d2
+      var cy = y + Math.random() * d - d2
+      var cz = z + Math.random() * d - d2
+
+      positions[i] = ax
+      positions[i + 1] = ay
+      positions[i + 2] = az
+
+      positions[i + 3] = bx
+      positions[i + 4] = by
+      positions[i + 5] = bz
+
+      positions[i + 6] = cx
+      positions[i + 7] = cy
+      positions[i + 8] = cz
+
+      //face normals
+      pA.set( ax, ay, az)
+      pB.set( bx, by, bz)
+      pC.set( cx, cy, cz)
+
+      cb.subVectors( pC, pB)
+      ab.subVectors( pA, pB)
+
+      cb.cross(ab)
+
+      cb.normalize()
+
+      var nx = cb.x
+      var ny = cb.y
+      var nz = cb.z
+
+      normals[i] = nx
+      normals[i + 1] = ny
+      normals[i + 2] = nz
+
+      normals[i + 3] = nx
+      normals[i + 4] = ny
+      normals[i + 5] = nz
+
+      normals[i + 6] = nx
+      normals[i + 7] = ny
+      normals[i + 8] = nz
+
+      //colors [0,1]
+      var vx = (x / n) + 0.5
+      var vy = (y / n) + 0.5
+      var vz = (z / n) + 0.5
+
+      color.setRGB(vx, vy, vz)
+
+      colors[i] = color.r
+      colors[i + 1] = color.g
+      colors[i + 2] = color.b
+
+      colors[i + 3] = color.r
+      colors[i + 4] = color.g
+      colors[i + 5] = color.b
+
+      colors[i + 6] = color.r
+      colors[i + 7] = color.g
+      colors[i + 8] = color.b
+    }
+    geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3))
+    geometry.addAttribute('normal', new THREE.BufferAttribute(normals, 3))
+    geometry.addAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+    geometry.computeBoundingSphere()
+
+    var material = new THREE.MeshPhongMaterial({
+      color: 0xaaaaaa, specular: 0xffffff, shininess: 250,
+      side: THREE.DoubleSide, vertexColors: THREE.VertexColors
+    })
+
+    mesh = new THREE.Mesh( geometry, material)
+    scene.add(mesh)*/
+
+    var geometry = new THREE.BoxGeometry(100, 100, 100)
+
+    for (var i = 0; i < geometry.faces.length; i += 2) {
+      var hex = Math.random() * 0xffffff
+      geometry.faces[i].color.setHex(hex)
+      geometry.faces[i + 1].color.setHex(hex)
+    }
+
+    var material = new THREE.MeshBasicMaterial({ vertexColors: THREE.FaceColors })
+    mesh = new THREE.Mesh( geometry, material)
+    //mesh.position = new THREE.Vector3(0, 0, 0)
     scene.add(mesh)
+
+  }
+
+  function initGrid () {
+    var helper = new THREE.GridHelper( 1000, 50, 0x0000ff, 0x808080 )
+    //helper.setColors(0x0000ff, 0x808080)
+    scene.add(helper)
   }
 
   function createUI () {
@@ -242,14 +384,17 @@ app.controller('loginCtrl', ['$scope',  '$rootScope', '$state', '$http', 'toaste
   }
 
   function animation () {
-    light.position.set(param.x, param.y, param.z)
-    //camera.position.x += 10;
-    //camera.position.z += 10;
+    //light.position.set(param.x, param.y, param.z)
+    var time = Date.now() * 0.001
+
+    mesh.rotation.x = time * 0.25;
+    mesh.rotation.y = time * 0.5;
     changeFov();
     change();
     renderer.clear()
     renderer.render(scene, camera);
     requestAnimationFrame(animation);
+    stats.update()
   }
 
   function setCameraFov (fov) {
